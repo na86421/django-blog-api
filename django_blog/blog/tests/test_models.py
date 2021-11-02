ef0466e26ba7208c0dc081d29a795a438a380230
JunKi Yoon <na66421@gmail.com>
2021-11-02 22:38:54 +0900

    add testcase for UserModel

--- django_blog/blog/tests/test_models.py
+++ django_blog/blog/tests/test_models.py
@@ -18,8 +18,10 @@ class UserModelTest(TestCase):
             user = self.user.objects.create(email='na66421@gmail.com', username='na66421',
                                             name='윤준기', password='qwer1234')
 
-        self.assertEqual(user.email, 'na66421@gmail.com')
-        self.assertEqual(user.name, '윤준기')
-        self.assertEqual(user.created_at, mock_date)
-        self.assertEqual(user.modified_at, mock_date)
-        self.assertEqual(str(user), user.name, "__str__ 테스트 실패")
+        self.assertEqual(user.email, 'na66421@gmail.com', "email 불일치")
+        self.assertEqual(user.username, 'na66421', "username 불일치")
+        self.assertEqual(user.name, '윤준기', "name 불일치")
+        self.assertEqual(user.password, 'qwer1234', "password 불일치")
+        self.assertEqual(user.created_at, mock_date, "생성시간 불일치")
+        self.assertEqual(user.modified_at, mock_date, "수정시간 불일치")
+        self.assertEqual(str(user), user.name, "__str__ 불일치")
