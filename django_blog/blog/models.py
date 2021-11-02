78a716937680a52d4784bebfe510dba97bd0e186
JunKi Yoon <na66421@gmail.com>
2021-11-02 22:36:06 +0900

    create user Model, add UnitTest

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Timestampable(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    class Meta:
+        abstract = True
+
+
+class User(AbstractUser, Timestampable):
+    name = models.CharField(max_length=200, blank=True, help_text='사용자 이름')
+
+    def __str__(self):
+        return self.name
---
+++ django_blog/blog/tests/test_models.py
@@ -0,0 +1,26 @@
+from unittest import mock
+
+from django.contrib.auth import get_user_model
+from django.utils import timezone
+from django.test import TestCase
+
+class UserModelTest(TestCase):
+    def setUp(self):
+        self.user = get_user_model()
+        
+    def tearDown(self):
+        del self.user
+
+    def test_default_values(self):
+        # pass
+        mock_date = timezone.now()
+        with mock.patch('django.utils.timezone.now') as mock_now:
+            mock_now.return_value = mock_date
+            user = self.user.objects.create(email='na66421@gmail.com', username='na66421',
+                                            name='윤준기', password='qwer1234')
+
+        self.assertEqual(user.email, 'na66421@gmail.com')
+        self.assertEqual(user.name, '윤준기')
+        self.assertEqual(user.created_at, mock_date)
+        self.assertEqual(user.modified_at, mock_date)
+        self.assertEqual(str(user), user.name, "__str__ 테스트 실패")
