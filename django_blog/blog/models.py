e895156dcc4d16f1b0ab147aaca9a546f3918d28
JunKi Yoon <na66421@gmail.com>
2021-11-03 19:34:53 +0900

    create Category, Post Model, add Unittest

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -10,7 +10,24 @@ class Timestampable(models.Model):
 
 
 class User(AbstractUser, Timestampable):
-    name = models.CharField(max_length=200, blank=True, help_text='사용자 이름')
+    name = models.CharField(max_length=200, help_text='사용자 이름')
 
     def __str__(self):
         return self.name
+
+
+class Category(Timestampable):
+    name = models.CharField(max_length=200, help_text='카테고리명')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, help_text='카테고리 생성한 사용자')
+
+    def __str__(self):
+        return f'{self.user.name}:{self.name}'
+
+class Post(Timestampable):
+    title = models.CharField(max_length=50, help_text='포스트 제목')
+    content = models.CharField(max_length=100, help_text='포스트 내용')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, help_text='속한 카테고리')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, help_text='포스트 생성한 사용자')
+
+    def __str__(self):
+        return f'{self.user.name}:{self.category.name}:{self.title}'
--- django_blog/blog/tests/test_models.py
+++ django_blog/blog/tests/test_models.py
@@ -4,24 +4,62 @@ from django.contrib.auth import get_user_model
 from django.utils import timezone
 from django.test import TestCase
 
+from blog.models import Category, Post
+
 class UserModelTest(TestCase):
     def setUp(self):
+        print(f'-------{self._testMethodName}-------')
         self.user = get_user_model()
         
     def tearDown(self):
         del self.user
 
-    def test_default_values(self):
+    def test_create_user(self):
         mock_date = timezone.now()
         with mock.patch('django.utils.timezone.now') as mock_now:
             mock_now.return_value = mock_date
             user = self.user.objects.create(email='na66421@gmail.com', username='na66421',
                                             name='윤준기', password='qwer1234')
 
-        self.assertEqual(user.email, 'na66421@gmail.com', "email 불일치")
-        self.assertEqual(user.username, 'na66421', "username 불일치")
-        self.assertEqual(user.name, '윤준기', "name 불일치")
-        self.assertEqual(user.password, 'qwer1234', "password 불일치")
-        self.assertEqual(user.created_at, mock_date, "생성시간 불일치")
-        self.assertEqual(user.modified_at, mock_date, "수정시간 불일치")
-        self.assertEqual(str(user), user.name, "__str__ 불일치")
+        self.assertEqual(user.email, 'na66421@gmail.com', 'email 불일치')
+        self.assertEqual(user.username, 'na66421', 'username 불일치')
+        self.assertEqual(user.name, '윤준기', 'name 불일치')
+        self.assertEqual(user.password, 'qwer1234', 'password 불일치')
+        self.assertEqual(user.created_at, mock_date, '생성시간 불일치')
+        self.assertEqual(user.modified_at, mock_date, '수정시간 불일치')
+        self.assertEqual(str(user), user.name, '__str__ 불일치')
+
+    def test_create_category(self):
+        mock_date = timezone.now()
+        with mock.patch('django.utils.timezone.now') as mock_now:
+            mock_now.return_value = mock_date
+            user = self.user.objects.create(email='na66421@gmail.com', username='na66421',
+                                            name='윤준기', password='qwer1234')
+            category = Category.objects.create(name='프로그래밍', user=user)
+
+        self.assertEqual(category.name, '프로그래밍', '카테고리 이름 불일치')
+        self.assertEqual(category.user, user, '카테고리 생성 사용자 불일치')
+        self.assertEqual(category.created_at, mock_date, '생성시간 불일치')
+        self.assertEqual(category.modified_at, mock_date, '수정시간 불일치')
+        self.assertEqual(str(category), f'{user.name}:{category.name}', '__str__ 불일치')
+
+    def test_create_post(self):
+        mock_date = timezone.now()
+        with mock.patch('django.utils.timezone.now') as mock_now:
+            mock_now.return_value = mock_date
+            user = self.user.objects.create(email='na66421@gmail.com', username='na66421',
+                                            name='윤준기', password='qwer1234')
+            category = Category.objects.create(name='프로그래밍', user=user)
+            post = Post.objects.create(title='프로그래밍', content='프로그래밍', category=category,
+                                       user=user)
+
+        self.assertEqual(post.title, '프로그래밍', '포스트 제목 불일치')
+        self.assertEqual(post.content, '프로그래밍', '포스트 내용 불일치')
+        self.assertEqual(post.user, user, '포스트 생성 사용자 불일치')
+        self.assertEqual(post.created_at, mock_date, '생성시간 불일치')
+        self.assertEqual(post.modified_at, mock_date, '수정시간 불일치')
+        self.assertEqual(str(post), f'{user.name}:{category.name}:{post.title}', '__str__ 불일치')
+
+
+
+        
