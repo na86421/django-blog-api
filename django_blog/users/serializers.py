7b4b7d1bfb994173cb763bffc9fc2ce61f17eeb3
JunKi Yoon <na66421@gmail.com>
2021-11-21 17:13:19 +0900

    Refactor SignupVIew that using generic.CreateAPIView

--- django_blog/users/serializers.py
+++ django_blog/users/serializers.py
@@ -1,10 +1,23 @@
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, password_validation
 
 from rest_framework import serializers
 
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
     class Meta:
-        User = get_user_model()
-        model = User
-        fields = ['id', 'username', 'name', 'created_at', 'modified_at', 'is_staff', 'is_superuser']
+        model = get_user_model()
+        fields = ['id', 'username', 'name', 'created_at', 'modified_at', 'is_staff', 'is_superuser', 'password']
+
+    def validate_password(self, value):
+        """
+        Check that the password is not simple.
+        """
+        password_validation.validate_password(value)
+        return value
+
+
+# TODO
+class SignInSerializer(serializers.Serializer):
+    pass
--- django_blog/users/views.py
+++ django_blog/users/views.py
@@ -1,6 +1,4 @@
 from django.contrib.auth import get_user_model, authenticate
-from django.contrib.auth.password_validation import validate_password
-from django.core.exceptions import ValidationError
 
 from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
@@ -15,43 +13,9 @@ from .serializers import UserSerializer
 User = get_user_model()
 
 
-class SignUpView(APIView):
+class SignUpView(generics.CreateAPIView):
     permission_classes = [permissions.AllowAny]
-
-    @swagger_auto_schema(request_body=openapi.Schema(
-        type=openapi.TYPE_OBJECT,
-        properties={
-            'username': openapi.Schema(type=openapi.TYPE_STRING, description='username'),
-            'name': openapi.Schema(type=openapi.TYPE_STRING, description='name'),
-            'password1': openapi.Schema(type=openapi.TYPE_STRING, description='password1, minLength:8'),
-            'password2': openapi.Schema(type=openapi.TYPE_STRING, description='password2 confirm, minLength:8'),
-        }))
-    def post(self, request, format=None):
-        data = request.data
-
-        try:
-            if data['password1'] != data['password2']:
-                return Response({'msg': '비밀번호가 일치하지 않습니다.'}, status=status.HTTP_400_BAD_REQUEST)
-            validate_password(data['password1'])
-
-            if User.objects.filter(username=data['username']).exists():
-                return Response({'msg': '이미 존재하는 username 입니다.'}, status=status.HTTP_400_BAD_REQUEST)
-
-            user = User.objects.create_user(
-                username=data['username'],
-                name=data['name'],
-                password=data['password1']
-            )
-            token = Token.objects.create(user=user)
-
-            user_data = {'token': token.key}
-            user_data.update(UserSerializer(user).data)
-        except KeyError:
-            return Response({'msg': '필수 입력항목을 입력해주세요.'}, status=status.HTTP_400_BAD_REQUEST)
-        except ValidationError:
-            return Response({'msg': '비밀번호를 수정해주세요.'}, status=status.HTTP_400_BAD_REQUEST)
-
-        return Response(user_data, status=status.HTTP_201_CREATED)
+    serializer_class = UserSerializer
 
 
 class SignInView(APIView):
@@ -83,7 +47,7 @@ class SignInView(APIView):
 
 class UserView(generics.RetrieveUpdateAPIView):
     """
-    UserView can only can only retrieve and update.
+    UserView can only retrieve and update.
     """
     queryset = User.objects.all()
     serializer_class = UserSerializer
