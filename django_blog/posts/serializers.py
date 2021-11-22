86859ebc89830635181129dcea5dedf407db6399
JunKi Yoon <na66421@gmail.com>
2021-11-22 20:45:34 +0900

    Refactor PostViewSet that using serializer validate

--- django_blog/posts/serializers.py
+++ django_blog/posts/serializers.py
@@ -1,5 +1,4 @@
 from rest_framework import serializers
-from rest_framework.exceptions import ValidationError
 
 from .models import Post
 
@@ -8,18 +7,3 @@ class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = '__all__'
-
-    def validate(self, data):
-        '''
-        check title, content, category field is required
-        '''
-        err_msg = {}
-        if 'title' not in data:
-            err_msg['title'] = ["This field is required."]
-        if 'content' not in data:
-            err_msg['content'] = ["This field is required."]
-        if 'category' not in data:
-            err_msg['category'] = ["This field is required."]
-        if bool(err_msg) is not False:
-            raise ValidationError(err_msg)
-        return data
