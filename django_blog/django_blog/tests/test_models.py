8cf81c53c62869054f65880f06fc2898d196212f
JunKi Yoon <na66421@gmail.com>
2021-11-28 15:05:24 +0900

    Refactoring to simplify test cases

--- django_blog/django_blog/tests/test_models.py
+++ django_blog/django_blog/tests/test_models.py
@@ -1,5 +1,3 @@
-from unittest import mock
-
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 
@@ -7,9 +5,41 @@ from categories.models import Category
 from posts.models import Post
 
 
+class UserTest(TestCase):
+    def setUp(self):
+        print(f'-------{self._testMethodName}-------')
+
+    def test_create_user(self):
+        user = get_user_model().objects.create_user(username='na66421', name='윤준기', password='qwer!@#$')
+
+        self.assertEqual(user.username, 'na66421')
+        self.assertEqual(user.name, '윤준기')
+        self.assertEqual(str(user), user.name, '__str__ 불일치')
+
+
+class CategoryTest(TestCase):
+    def setUp(self):
+        print(f'-------{self._testMethodName}-------')
+
+    def test_create_category(self):
+        user = get_user_model().objects.create_user(username='na66421', name='윤준기', password='qwer!@#$')
+        category = Category.objects.create(name='python', user=user)
+
+        self.assertEqual(category.name, 'python')
+        self.assertEqual(str(category), f'{user.name}:{category.name}', '__str__ 불일치')
+
+
 class PostTest(TestCase):
     def setUp(self):
         print(f'-------{self._testMethodName}-------')
+
+    def test_create_post(self):
+        user = get_user_model().objects.create_user(username='na66421', name='윤준기', password='qwer!@#$')
+        category = Category.objects.create(name='python', user=user)
+        post = Post.objects.create(title='title', content='content', category=category, user=user)
+
+        self.assertEqual(post.title, 'title')
+        self.assertEqual(str(post), f'{user.name}:{category.name}:{post.title}', '__str__ 불일치')
 
     def test_post_increase_hits(self):
         user = get_user_model().objects.create(email='na66421@gmail.com', username='na66421',
--- django_blog/django_blog/tests/test_users_views.py
+++ django_blog/django_blog/tests/test_users_views.py
@@ -8,104 +8,48 @@ from .base_client import connectAPITest
 class SignUpViewTest(connectAPITest):
     def setUp(self):
         super().setUp()
-        self.data = {
+        self.signup_data = {
             'username': 'test',
             'name': '테스트',
-            'password1': 'qwer!@#$',
-            'password2': 'qwer!@#$',
+            'password': 'qwer!@#$'
         }
 
     def test_signup(self):
-        res = self.client.post('/accounts/signup/', json.dumps(self.data), content_type="application/json")
+        res = self.client.post('/api/v1/users/signup/', json.dumps(self.signup_data), content_type="application/json")
         self.assertEqual(res.status_code, 201)
 
-    def test_signup_invalid_data(self):
-        self.data = {}
+    def test_signup_with_simple_pasword(self):
+        self.signup_data['password'] = 'qwer1234'
 
-        res = self.client.post('/accounts/signup/', json.dumps(self.data), content_type="application/json")
-        self.assertEqual(res.status_code, 400)
-
-    def test_signup_invalid_password(self):
-        self.data['password1'] = ''
-        self.data['password2'] = ''
-
-        res = self.client.post('/accounts/signup/', json.dumps(self.data), content_type="application/json")
-        self.assertEqual(res.status_code, 400)
-
-    def test_signup_not_eqaul_password(self):
-        self.data['password1'] = 'qwer1234'
-
-        res = self.client.post('/accounts/signup/', json.dumps(self.data), content_type="application/json")
-        self.assertEqual(res.status_code, 400)
-
-    def test_signup_already_exists(self):
-        self.data['username'] = 'na66421'
-
-        res = self.client.post('/accounts/signup/', json.dumps(self.data), content_type="application/json")
+        res = self.client.post('/api/v1/users/signup/', json.dumps(self.signup_data), content_type="application/json")
         self.assertEqual(res.status_code, 400)
 
 
 class SignInViewTest(connectAPITest):
     def setUp(self):
         super().setUp()
-        self.data = {
+        self.signin_data = {
             'username': self.user.username,
             'password': 'qwer!@#$',
         }
 
     def test_signin(self):
-        res = self.client.post('/accounts/signin/', json.dumps(self.data), content_type="application/json")
+        res = self.client.post('/api/v1/users/signin/', json.dumps(self.signin_data), content_type="application/json")
         self.assertEqual(res.status_code, 200)
 
-    def test_signin_invalid_data(self):
-        self.data = {}
-
-        res = self.client.post('/accounts/signin/', json.dumps(self.data), content_type="application/json")
-        self.assertEqual(res.status_code, 400)
-
     def test_signin_invalid_password(self):
-        self.data['password'] = 'qwer1234'
-
-        res = self.client.post('/accounts/signin/', json.dumps(self.data), content_type="application/json")
+        self.signin_data['password'] = '3434'
+        res = self.client.post('/api/v1/users/signin/', json.dumps(self.signin_data), content_type="application/json")
         self.assertEqual(res.status_code, 401)
 
 
 class UserViewSetTest(connectAPITest):
-    def setUp(self):
-        super().setUp()
-        self.user2 = get_user_model().objects.create_user(
-            username='na664212', name='윤준기2',
-            password='qwer!@#$', is_staff=True
-        )
-        self.data = {
-            'name': 'name',
-            'password': 'qwer!@#$'
+    def test_update_user_has_not_permission(self):
+        test_user = get_user_model().objects.create_user(username='testuser', name='testuser', password='qwer!@#$')
+        update_data = {
+            'name': 'Newtestuser'
         }
 
-    def test_update_user(self):
-        res = self.client.patch(f'/api/v1/users/{self.user.id}/', json.dumps(self.data),
+        res = self.client.patch(f'/api/v1/users/{test_user.id}/', json.dumps(update_data),
                                 content_type="application/json")
-        self.assertEqual(res.status_code, 200)
-        self.assertEqual(res.json()['name'], self.data['name'], '프로필 수정 실패')
-
-    def test_update_invalid_data(self):
-        self.data = {}
-
-        res = self.client.patch(f'/api/v1/users/{self.user.id}/', json.dumps(self.data),
-                                content_type="application/json")
-        self.assertEqual(res.status_code, 400)
-
-    def test_update_invalid_password(self):
-        self.data['password'] = ''
-
-        res = self.client.patch(f'/api/v1/users/{self.user.id}/', json.dumps(self.data),
-                                content_type="application/json")
-        self.assertEqual(res.status_code, 400)
-
-    def test_update_not_me(self):
-        res = self.client.patch(f'/api/v1/users/{self.user2.id}/', json.dumps(self.data),
-                                content_type="application/json")
-        self.assertEqual(res.status_code, 400)
-
-
-
+        self.assertEqual(res.status_code, 403)
