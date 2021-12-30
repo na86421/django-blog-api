52902e65fcab79f566cab011daa0215d4ef6b9b4
JunKi Yoon <na66421@gmail.com>
2021-12-30 19:46:22 +0900

    Fix duplicate def update in CommentSerializer

--- django_blog/comments/serializers.py
+++ django_blog/comments/serializers.py
@@ -8,8 +8,3 @@ class CommentSerializer(DisableUpdateUserMixin, serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = '__all__'
-
-    def update(self, instance, validated_data):
-        if instance.user != validated_data.get('user', instance.user):
-            raise serializers.ValidationError("You cannot change users.")
-        return super().update(instance, validated_data)
